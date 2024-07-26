eaf30c870c45af14e33fdbe1c1422042cf043b56
Miltos Koniaris <koniaris.miltos@gmail.com>
2024-07-26 19:06:11 +0300

    polling for continuous testing

--- main.py
+++ main.py
@@ -8,12 +8,10 @@ from pages.live_program_page import LiveProgramPage
 from pages.betting_live_page import BettingLivePage
 from setup import setup_driver
 
-st = time.time()
-
 def main():
-
     driver = setup_driver()
     try:
+        st = time.time()
         driver.get(urls.live_schedule_url)
         live_program_page_instance = LiveProgramPage(driver)
         live_program_page_instance.close_login_popup().accept_cookies().change_language("English")
@@ -26,18 +24,15 @@ def main():
 
         report_generator = ReportGeneratorXML()
         report_generator.create_report()
-    except Exception as e:
-        print(f'Message: {e}')
-        traceback.print_exc()
-        print('[FAIL] see screenshot for details')
+    except:
         driver.get_screenshot_as_file("./data/screenshot.png")
         logging.error(f'Run failed because of:\n {traceback.print_exc()} \n  screenshot has been saved for review')
     finally:
         logging.info(f"Execution time: {time.time() - st:.2f} seconds")
         driver.quit()
-        exec_time = time.time() - st
-        print(f"Execution time: {exec_time:.2f} seconds")
 
 
 if __name__ == "__main__":
-    main()
+    # change step for your desired interval (in seconds)
+    polling.poll(main, step=60, poll_forever=True)
+    
