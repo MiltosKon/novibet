f13771bf80e4334b7aff33047709e0930010e005
Miltos Koniaris <koniaris.miltos@gmail.com>
2024-07-26 15:56:33 +0300

    temp fix: issue with webdriver_manager package

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ def setup_driver():
     options.add_argument("--no-sandbox")
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     options.add_argument(f"user-agent={user_agent}")
-
-    driver = webdriver.Chrome(service=Service( ChromeDriverManager().install()), options=options)
+    driver_path = ChromeDriverManager().install().replace("THIRD_PARTY_NOTICES.chromedriver", "chromedriver.exe")
+    driver = webdriver.Chrome(service=Service(driver_path ), options=options)
     return driver
